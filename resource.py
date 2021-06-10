b182d7368ed10629fb0c52f38a94cd3dcc4ec1a4
RoyEden <roy.ede@gmail.com>
2021-06-10 02:40:15 -0300

    Added files

--- resource.py
+++ resource.py
@@ -1,10 +1,28 @@
 # DB
 from models.resource import Resource, ResourceSchema
 
+
 def get_all():
     result = Resource.query.order_by(Resource.name).all()
     return ResourceSchema(many=True).dump(result), 200
 
+
 def get_resource(resource_id):
     result = Resource.query.get_or_404(resource_id)
     return ResourceSchema().dump(result), 200
+
+
+def create_resource(file):
+    print(file)
+    return {}, 200
+
+
+def get_file(filename):
+    import flask
+    return flask.send_from_directory("storage", filename), 200
+
+
+def resize_file(filename):
+    import flask
+    file = flask.send_from_directory("storage", filename)
+    return {}, 200
